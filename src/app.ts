60f29d3f02b1797c5648c2cdd75f55e192157d72
Marek <s.marekszwed@gmail.com>
2024-08-02 21:58:42 +0200

    Checking inputs value /adding cards

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,103 @@
+import axios from 'axios';
+import Toastify from 'toastify-js';
+
 const modal = document.querySelector('.dialog') as HTMLDivElement;
 const game = document.querySelector('.js-app') as HTMLDivElement;
+const gameContainer = document.querySelector('.js-game') as HTMLDivElement;
+const nameInput = document.querySelector('.js-input-name') as HTMLInputElement;
+// Buttons
 const playButton = document.querySelector(
 	'.js-play-button'
 ) as HTMLButtonElement;
-const nameINput = document.querySelector('.js-input-name') as HTMLInputElement;
-const difficultyInputs = document.querySelectorAll('.js-check-input');
+const restartButton = document.querySelector(
+	'.js-restart-game'
+) as HTMLButtonElement;
+// Hidden class
+const hiddenClass: string = 'hidden';
+// URL
+const API_KEY = '28be1067';
+const API_LINK = 'http://www.omdbapi.com/?t=Star+Wars&apikey=';
+const URL = API_LINK + API_KEY;
+
+let numberOfImage: number;
 
 const closeModal = () => {
-	if (nameINput.value !== '') {
-		modal.classList.add('hidden');
-		game.classList.remove('hidden');
-		setDifficulty();
+	if (nameInput.value) {
+		modal.classList.add(hiddenClass);
+		game.classList.remove(hiddenClass);
 	} else {
-		console.log('empty name space');
+		Toastify({
+			text: 'Please add your nickname',
+			duration: 3000,
+			newWindow: true,
+			close: true,
+			gravity: 'top', // `top` or `bottom`
+			position: 'center', // `left`, `center` or `right`
+			stopOnFocus: true, // Prevents dismissing of toast on hover
+			style: {
+				background: 'linear-gradient(to right, #00b09b, #96c93d)',
+			},
+			onClick: function () {}, // Callback after click
+		}).showToast();
 	}
 };
 
-const setDifficulty = () => {};
+const setDifficulty = () => {
+	const selectedInput = document.querySelector(
+		'input[name="choose"]:checked'
+	) as HTMLInputElement;
 
-document.addEventListener('DOMContentLoaded', setDifficulty);
-playButton.addEventListener('click', (e) => {
-	e.preventDefault();
-	closeModal();
+	if (selectedInput) {
+		const selectedValue = selectedInput.value;
+		localStorage.setItem('selectedInput', selectedValue);
+	} else {
+		console.log('Nothing clicked');
+	}
+};
+
+const setGrid = () => {
+	const chosenValue = localStorage.getItem('selectedInput');
+	console.log(chosenValue);
+
+	switch (chosenValue) {
+		case 'easy':
+			numberOfImage = 8;
+			break;
+		case 'medium':
+			numberOfImage = 16;
+			break;
+		case 'hard':
+			numberOfImage = 20;
+			break;
+		default:
+			numberOfImage = 8;
+	}
+
+	addItems();
+};
+
+async function fetchData() {
+	await axios.get(URL).then((res) => {
+		console.log(res.data);
+	});
+}
+// checking axios response
+fetchData();
+
+const addItems = () => {
+	const liItem = document.createElement('li');
+	const imgItem = document.createElement('img');
+	const blankItem = document.createElement('div');
+	blankItem.classList.add('blank-background');
+	liItem.append(imgItem, blankItem);
+	gameContainer.appendChild(liItem);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	playButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		closeModal();
+		setDifficulty();
+		setGrid();
+	});
 });
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,3 @@
 import './style.scss';
 import './app.ts';
 
-
-
-
-
